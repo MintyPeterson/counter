cd8646d7ee36d3a27af8f1b991da8861e6e9cf50
Tom cook <tom.cook@ymail.com>
2022-12-28 12:01:11 +0000

    Added integration tests for an entry with notes.

---
+++ src/api/MintyPeterson.Counter.Api.Tests/Integration/Functions/Entry/EditEntryWithNotesTest.cs
@@ -0,0 +1,126 @@
+﻿// <copyright file="EditEntryWithNotesTest.cs" company="Tom Cook">
+// Copyright (c) Tom Cook. All rights reserved.
+// </copyright>
+
+namespace MintyPeterson.Counter.Api.Tests.Integration.Functions.Entry
+{
+  using System.Net;
+  using System.Net.Http.Json;
+  using FluentAssertions;
+  using MintyPeterson.Counter.Api.Models.Requests;
+  using MintyPeterson.Counter.Api.Models.Responses;
+  using Xunit;
+
+  /// <summary>
+  /// Tests if the user can edit an existing entry while including entry notes.
+  /// </summary>
+  public class EditEntryWithNotesTest : IntegrationTest
+  {
+    /// <summary>
+    /// Stores the <see cref="HttpResponseMessage"/>.
+    /// </summary>
+    private HttpResponseMessage? response;
+
+    /// <summary>
+    /// Stores the <see cref="HttpResponseMessage"/> content.
+    /// </summary>
+    private EntryEditResponse? responseContent;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EditEntryWithNotesTest"/> class.
+    /// </summary>
+    /// <param name="fixture">A <see cref="CounterWebApplicationFactory"/>.</param>
+    public EditEntryWithNotesTest(CounterWebApplicationFactory fixture)
+      : base(fixture)
+    {
+    }
+
+    /// <summary>
+    /// Tests if the status code is OK (200).
+    /// </summary>
+    [Fact]
+    public void StatusCodeShouldBeOk() =>
+      this.response!.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    /// <summary>
+    /// Tests if the entry identifier is not empty.
+    /// </summary>
+    [Fact]
+    public void EntryIdentiferShouldNotBeEmpty() =>
+      this.responseContent!.EntryId.Should().NotBeEmpty();
+
+    /// <inheritdoc/>
+    public override Task InitializeAsync()
+    {
+      return this.SendRequestAsync();
+    }
+
+    /// <summary>
+    /// Sends a request to the system.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    private async Task SendRequestAsync()
+    {
+      await this.ResetAndPopulateDatabaseAsync(
+        @"
+          DECLARE @CurrentDateTime datetimeoffset = SYSDATETIMEOFFSET()
+          DECLARE @CurrentDate date = GETDATE()
+          DECLARE @UserID nvarchar(450) = '00000000-0000-0000-0000-000000000001'
+          DECLARE @EntryID nvarchar(450) = '00000000-0000-0000-0000-000000000001'
+
+          INSERT INTO Users (
+            UserID
+           ,CreatedDateTime
+           ,UpdatedDateTime
+           ,Name
+           ,Email
+          )
+          VALUES (
+            @UserID
+           ,@CurrentDateTime
+           ,@CurrentDateTime
+           ,'Integration Tester'
+           ,'identity.tester@drtomcook.uk'
+          )
+
+          INSERT INTO Entries (
+            EntryID
+           ,CreatedDateTime
+           ,CreatedByUserID
+           ,UpdatedDateTime
+           ,UpdatedByUserID
+           ,EntryDate
+           ,Entry
+           ,Notes
+          )
+          VALUES (
+            @EntryID
+           ,@CurrentDateTime
+           ,@UserID
+           ,@CurrentDateTime
+           ,@UserID
+           ,@CurrentDate
+           ,'10'
+           ,'Ten'
+          )
+        ");
+
+      var content = BuildRequestContent(
+        new EntryEditRequestBody
+        {
+          EntryDate = DateTime.Today.AddDays(1),
+          Entry = 20,
+          Notes = "Twenty",
+        });
+
+      this.response =
+        await this.Client.PutAsync("/Entry/00000000-0000-0000-0000-000000000001", content);
+
+      if (this.response.IsSuccessStatusCode)
+      {
+        this.responseContent =
+          await this.response.Content.ReadFromJsonAsync<EntryEditResponse>();
+      }
+    }
+  }
+}
